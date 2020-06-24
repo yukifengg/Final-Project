6aa1339a5ec14f199d69a2b12b281edfd69c3bff
Sharon Zou <szou00@stuy.edu>
2020-06-24 17:56:20 -0400

    worked on wrongfully accused vcs

---
+++ FInal Project/AccusedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AccusedViewController.swift
+//  Final Project
+//
+//  Created by Sharon Zou on 6/24/20.
+//  Copyright © 2020 Yuki Feng. All rights reserved.
+//
+
+import UIKit
+
+class AccusedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FInal Project/VocabularyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VocabularyViewController.swift
+//  Final Project
+//
+//  Created by Sharon Zou on 6/24/20.
+//  Copyright © 2020 Yuki Feng. All rights reserved.
+//
+
+import UIKit
+
+class VocabularyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closeVocab(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
